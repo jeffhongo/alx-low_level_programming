3cbdd0cc60fc27cebd2ba690b1e5450860816f0e
jeffhongo <jefhongo@gmail.com>
2022-09-08 14:39:32 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - main block
- * return:0
+ * Return:0
 */
-int main (void)
-{ 
-printf("with proper grammar, nut the outcome is a price of art,\n");
-return(0);
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
