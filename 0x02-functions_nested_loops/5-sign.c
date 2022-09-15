44178bce00208bbca34553a6eb9b1cea162a7dee
jeffhongo <jefhongo@gmail.com>
2022-09-15 09:18:04 +0000

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,21 @@
 /**
  *  print_sign - prints the soign of a number
  *  @n: this is the character that is to be checked
- *  Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero,-1 and prints - if n is less than zero
+ *  Return: 1 and prints +, 0 and prints 0, 1 and prints -
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0);
+	else if (n == 0)
 	{
 		_putchar('0');
-		return (0)
+		return (0);
 	}
-	else if (n < );
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
